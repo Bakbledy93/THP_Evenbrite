0f730a119820b0956b9c8377afa6229e8576507d
bakbledy93 <phim.kevin@gmail.com>
2020-08-10 14:03:43 +0200

    first commit - User

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :events
+  has_many :events, through: :attendances
+
+  has_many :events, foreign_key: 'admin_id', class_name: "Event"
+  has_many :attendances, foreign_key: 'participant_id', class_name:"Attendance"
+end
