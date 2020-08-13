4971b7abae9c7770cc00735e84c9a65d4421e74e
bakbledy93 <phim.kevin@gmail.com>
2020-08-13 17:11:29 +0200

    eleventh commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root 'events#index' 
   
   resources :events
-  resources :users
+  resources :users, only: [:show] do
+    resources :avatars, only: [:create]
+  end
   resources :charges
   resources :attendances
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
